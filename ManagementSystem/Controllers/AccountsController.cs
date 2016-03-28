395a1e2edab2a3e56a8fc4083c212a0128d9a3bf
Todor Ivanov <Todor Ivanov>
2016-03-28 09:55:40 +0300

    Added authentication service and filter

---
+++ ManagementSystem/Controllers/AccountsController.cs
@@ -0,0 +1,44 @@
+﻿using ManagementSystem.Services;
+using ManagementSystem.ViewModels.AccountsVM;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ManagementSystem.Controllers
+{
+    public class AccountsController : Controller
+    {
+
+        public ActionResult Login(string RedirectUrl)
+        {
+            AccountLoginVM model = new AccountLoginVM();
+            model.RedirectUrl = RedirectUrl;
+
+            return View(model);
+        }
+        [HttpPost]
+        public ActionResult Login()
+        {
+            AccountLoginVM model = new AccountLoginVM();
+            TryUpdateModel(model);
+
+            AuthenticationService.AuthenticateUser(model.Username, model.Password);
+            if (AuthenticationService.LoggedUser != null)
+            {
+                if (!String.IsNullOrEmpty(model.RedirectUrl))
+                    return Redirect(model.RedirectUrl);
+
+                return RedirectToAction("Index", "Home");
+            }
+            return View(model);
+        }
+        public ActionResult Logout()
+        {
+            AuthenticationService.Logout();
+
+            return RedirectToAction("Login");
+        }
+    }
+}
---
+++ ManagementSystem/Services/AuthenticationService.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using ManagementSystem.Models;
+using ManagementSystem.Repositories;
+
+namespace ManagementSystem.Services
+{
+    public static class AuthenticationService
+    {
+        public static User LoggedUser { get; set; }
+
+        public static void AuthenticateUser(string username, string password)
+        {
+            LoggedUser = new UsersRepository().GetAll().FirstOrDefault(u => u.Username == username && u.Password == password);
+        }
+
+        public static void Logout()
+        {
+            LoggedUser = null;
+        }
+    }
+}
